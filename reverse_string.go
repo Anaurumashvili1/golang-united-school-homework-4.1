a5fee69c3d5b0d8c31dd471452c994b7deadee3f
Anaurumashvili1 <anaurumashvili@gmailcom>
2022-07-09 12:14:22 +0400

    first

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,9 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
-	return output
+	byteString := []rune(input)
+	for i, j := 0, len(byteString)-1; i < j; i, j = i+1, j-1 {
+		byteString[i], byteString[j] = byteString[j], byteString[i]
+	}
+	return string(byteString)
 }
